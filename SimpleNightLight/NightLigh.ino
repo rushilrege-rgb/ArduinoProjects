5ea56139768ce31ae77219bebebdb6eaba72eb7b
rushilrege-rgb <rushilrege@gmail.com>
2026-09-03 13:16:24 -0400

    Implement night light functionality with LED control
    Add functionality to read from a pin and control an LED based on the analog input value.

--- SimpleNightLight/NightLigh.ino
+++ SimpleNightLight/NightLigh.ino
@@ -1,1 +1,29 @@
+int readPin = A5;
+int readVal;
+int red = 8;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(readPin, INPUT);
+  pinMode(red, OUTPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  readVal = analogRead(readPin);
+  Serial.println(readVal);
+
+
+  while(readVal < 300){
+    digitalWrite(red, HIGH);
+    readVal = analogRead(readPin);
+  }
+
+  digitalWrite(red, LOW);
+}
+
+
+
 
