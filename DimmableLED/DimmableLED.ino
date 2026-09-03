73828934762e0f9d649b1f08f398918eec2137b5
rushilrege-rgb <rushilrege@gmail.com>
2026-09-02 21:15:03 -0400

    Implement LED brightness control based on analog input
    Read analog input to control green and red LED brightness.

--- DimmableLED/DimmableLED.ino
+++ DimmableLED/DimmableLED.ino
@@ -1,1 +1,37 @@
+int readpin = A5;
+int readval;
+float analogOutG;
+float analogOutR;
+float v;
+int dt = 10;
+int Gled = 3;
+int Rled = 6;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(readpin, INPUT);
+  pinMode(Gled, OUTPUT);
+  pinMode(Rled, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  readval = analogRead(readpin);
+  v = (5./1023.)*readval;
+  
+  
+  analogOutG = (255./1023.)*readval;
+  analogWrite(Gled, analogOutG);
+
+  analogOutR = (255./1023.)*readval;
+  analogWrite(Rled, analogOutR);
+
+  Serial.println(analogOutG *(5./255.));
+  delay(dt);
+
+
+
+}
 
