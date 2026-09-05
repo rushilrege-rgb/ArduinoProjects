9cc88cffd7f8827a3d2f2b64bd0b85a8943ae077
Rushil Rege <rushilrege@gmail.com>
2026-09-05 19:09:51 -0400

    Add BitFlipper Arduino sketch

---
+++ BitFlipper/BitFlipper.ino
@@ -0,0 +1,27 @@
+int latchPin = 11;
+int clockPin = 9;
+int dataPin = 12;
+int dt = 500;
+
+byte LEDs = 0b11110000;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  pinMode(latchPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, LSBFIRST, LEDs);
+  digitalWrite(latchPin, HIGH);
+  
+  LEDs = 255-LEDs;
+  delay(dt);
+
+}
